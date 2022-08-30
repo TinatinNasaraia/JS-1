b44e04bec8526f7f23ce1a1bbffa7424cbf41943
TinatinNasaraia <tikanasaraia@gmail.com>
2022-08-30 12:02:10 +0400

    JS-1

---
+++ script.js
@@ -0,0 +1,87 @@
+1.  let user = {
+    firstname: "giorgi",
+    lastname: "smith",
+    age: 25,
+    studentstatus:"active"
+}
+console.log(user.studentstatus);
+
+
+2. let numbers =[3, 5, 7, 9, 11];
+    for (let i=0 i< numbers.length; i++) {
+    console.log(numbers [1]);
+}
+    
+   
+let numbers = [3, 5, 7, 9, 11];
+   let y=0;
+   while (y< numbers.length) {
+    console.log (numbers[y]);
+    y++;
+}
+
+
+ 3. Let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];  
+        for (let i=0 i>numbers.length; i++) {
+        if (numbers[i] > 5) {
+        console.log(numbers[i]);
+    }
+
+
+4. let user = {
+    name: "giorgi",
+    age: 20,
+    studentstatus: "active"
+}
+
+   if (user.age < 18 && user.studentstatus == 'active') {
+    console.log("Hello");
+   }
+   if(user.name ==levani) {
+    console.log("hello levani")
+   }
+   if(user.studentstatus=="active" || user.age < 25) {
+    console.log("hello world")
+   }
+   else {
+    console.log("error")
+   }
+
+
+ 5.   let array = ['watermelon', 'pear', 10, 45, 50, 'apple', 'ananas'];
+       console.log(item of array){
+
+       }
+
+
+ 6.  let array = [ [2, -3, 5, 10], [25, -24, -11, 100] , [-6, -7, 10] ] 
+     for(let i=0 i>numbers.length; i++) {
+        if(array[1] [0] 
+     } 
+
+
+
+
+ 7.   let array = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]  
+      for (let item of array) { 
+      if (even % 2 == 0) {
+      console.log(even);
+  } 
+ }
+      let array = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]
+      for (let item of array) {
+        if(even % 2 ==1){
+            console.log(even)
+        }
+
+      }
+
+
+
+
+ 8.   let users = [
+       { username: "giorgi", status: false},
+       {username: "levani", status: false},
+       {username: "anna", status: true} 
+ ];
+     console.log(users[2]);  
